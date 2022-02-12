da59d830783331119a68ce74f92e9fbd9f839cbb
vampire_pp_ <vampirepp11@gmail.com>
2022-02-12 18:09:51 +0700

    first commit

--- src/routes/staffRoute.js
+++ src/routes/staffRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const staffController = require('../controllers/staffController');
 
+
+
 router.post("/addstaff",staffController.addstaff);
 router.post("/login",staffController.login);
 
